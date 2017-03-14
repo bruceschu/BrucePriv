8fa5bd3b99e7ccd1dea4c3bf795f2e824f9bd6ac
Bruce SCHU <xaustie@gmail.com>
2017-03-14 15:53:38 +0000

    added C (bruceschu)

--- src/Main.java
+++ src/Main.java
@@ -12,5 +12,6 @@ public class Main {
 	    System.out.println("gnguser 5");
 	    System.out.println("brucescu added A");
 	    System.out.println("brucescu added B");
+	    System.out.println("brucescu added C");
     }
 }
